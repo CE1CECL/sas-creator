2063448829f72481d97038b19a6800b729abed13
Christopher Lentocha <christopherericlentocha@gmail.com>
2025-12-24 13:39:19 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -16,9 +16,9 @@ targetArch=64
 
 [ -z "$ANDROID_BUILD_TOP" ] && ANDROID_BUILD_TOP=/build2/AOSP-11.0/
 if [ "$targetArch" == 32 ];then
-    srcFile="$ANDROID_BUILD_TOP/out/target/product/phhgsi_arm_ab/system.img"
+    srcFile="$ANDROID_BUILD_TOP/out/target/product/tdgsi_arm_ab/system.img"
 else
-    srcFile="$ANDROID_BUILD_TOP/out/target/product/phhgsi_arm64_ab/system.img"
+    srcFile="$ANDROID_BUILD_TOP/out/target/product/tdgsi_arm64_ab/system.img"
 fi
 if [ -f "$2" ];then
     srcFile="$2"
